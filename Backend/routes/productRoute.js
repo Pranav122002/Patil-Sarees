1296b43230e919799a813fc4b4f6888f35dac8f9
Pranav122002 <Pranavpatil122002@gmail.com>
2022-07-03 16:16:54 +0530

    initial commit

---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProductDetails,
+    createProductReview,
+    getProductReviews,
+    deleteReview,
+    getAdminProducts,
+  } = require("../controllers/productController");
+
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+
+
+module.exports = router;
